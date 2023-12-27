1632e4b1e9a6e99b2228f3b5dbe17bcb7d5ebf3f
Nitesh Chaurasiya <nitesh.chaurasiya@antiersolutions.com>
2023-12-27 21:42:42 +0530

    updated changes

--- app/class.js
+++ app/class.js
@@ -2,5 +2,12 @@ class made {
     async function getnumber(){
         // you are great you have made all mistakes
         return Promise.all[{a:123,b:678}]
+         
+        return Promise.all[]
+
+    }
+
+    function getdecimal(){
+        console.log('jbdsvn')
     }
 }
